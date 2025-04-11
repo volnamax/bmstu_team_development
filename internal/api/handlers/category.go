90c434dc32ac55eea2a8e36507c7e0225f9e77d8
Andrew Razin <razin.andrew.ru@mail.ru>
2025-04-11 12:55:59 +0300

    categories handlers[+]: init commit

--- internal/api/handlers/category.go
+++ internal/api/handlers/category.go
@@ -1,8 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"time"
+	"todolist/internal/models"
+	"todolist/internal/pkg/response"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"github.com/google/uuid"
 )
 
@@ -19,6 +25,14 @@ type CategoriesResponse struct {
 	Categories []CategoryResponse `json:"categories"`
 }
 
+type CategoriesProvider interface {
+	CreateCategory(ctx context.Context, category *models.CategoryBody) error
+	Delete(ctx context.Context, id uuid.UUID) error
+	GetByID(ctx context.Context, id uuid.UUID) (*models.Category, error)
+	GetByTaskID(ctx, id uuid.UUID) ([]models.Category, error)
+	GetAll(ctx context.Context, pageIndex, recordsPerPage int) ([]models.Category, error)
+}
+
 // @Summary CreateCategory
 // @Security ApiKeyAuth
 // @Tags category
@@ -28,13 +42,32 @@ type CategoriesResponse struct {
 // @Produce  json
 // @Param input body CategoryBody true "category name"
 // @Success 200
-// @Failure 400,404 {object} response.Response
+// @Failure 400 {object} response.Response
 // @Failure 500 {object} response.Response
 // @Failure default {object} response.Response
 // @Router /api/v1/category [post]
-func CreateCategory() http.HandlerFunc {
+func CreateCategory(categoryProvider CategoriesProvider, timeout time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req CategoryBody
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		category := models.CategoryBody{Name: req.Name}
 
+		ctx := r.Context()
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		err = categoryProvider.CreateCategory(ctx, &category)
+		if err != nil {
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		render.Status(r, http.StatusOK)
 	}
 }
 
@@ -51,9 +84,27 @@ func CreateCategory() http.HandlerFunc {
 // @Failure 500 {object} response.Response
 // @Failure default {object} response.Response
 // @Router /api/v1/category/{id} [delete]
-func DeleteCategory() http.HandlerFunc {
+func DeleteCategory(categoryProvider CategoriesProvider, timeout time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		uuid, err := uuid.Parse(id)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error("invalid UUID"))
+			return
+		}
 
+		ctx := r.Context()
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+
+		err = categoryProvider.Delete(ctx, uuid)
+		if err != nil {
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		render.Status(r, http.StatusOK)
 	}
 }
 
@@ -70,8 +121,41 @@ func DeleteCategory() http.HandlerFunc {
 // @Failure 500 {object} response.Response
 // @Failure default {object} response.Response
 // @Router /api/v1/category/all [post]
-func GetCategories() http.HandlerFunc {
+func GetCategories(categoryProvider CategoriesProvider, timeout time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req Pagination
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
 
+		ctx := r.Context()
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		var categories []models.Category
+		categories, err = categoryProvider.GetAll(ctx, req.PageIndex, req.RecordsPerPage)
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, toCategoriesResponse(categories))
+	}
+}
+
+func toCategoryResponse(category models.Category) CategoryResponse {
+	return CategoryResponse{
+		ID: category.ID,
+		CategoryBody: CategoryBody{
+			Name: category.Name,
+		},
+	}
+}
+
+func toCategoriesResponse(categories []models.Category) CategoriesResponse {
+	responses := make([]CategoryResponse, 0, len(categories))
+	for _, category := range categories {
+		responses = append(responses, toCategoryResponse(category))
+	}
+	return CategoriesResponse{
+		Categories: responses,
 	}
 }
