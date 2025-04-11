45b1646787971a08a96ffa063b18e2fdb5c01470
Andrew Razin <razin.andrew.ru@mail.ru>
2025-04-11 10:22:45 +0300

    users handlers[+]: init commit

--- internal/api/handlers/user.go
+++ internal/api/handlers/user.go
@@ -1,6 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+	"todolist/internal/models"
+	auth_utils "todolist/internal/pkg/authUtils"
+	"todolist/internal/pkg/response"
+
+	"github.com/go-chi/render"
+)
 
 type UserInfo struct {
 	Name     string `json:"name"`
@@ -9,6 +19,18 @@ type UserInfo struct {
 
 type Token struct {
 	Token string `json:"token"`
+}
+
+type AuthProvider interface {
+	SignIn(candidate *models.UserAuth) (tokenStr string, err error)
+	SignUp(candidate *models.UserAuth) error
+}
+
+func FromUserInfo(userDTO UserInfo) *models.UserAuth {
+	return &models.UserAuth{
+		Name:     userDTO.Name,
+		Password: userDTO.Password,
+	}
 }
 
 // @Summary SignIn
@@ -23,9 +45,30 @@ type Token struct {
 // @Failure 500 {object} response.Response
 // @Failure default {object} response.Response
 // @Router /api/v1/sign-in [post]
-func SignIn() http.HandlerFunc {
+func SignIn(authProvider AuthProvider, timeout time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req UserInfo
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
 
+		ctx := r.Context()
+		ctx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		tokenStr, err := authProvider.SignIn(FromUserInfo(req))
+		if err != nil {
+			if errors.Is(err, auth_utils.ErrInvalidToken) {
+				render.Status(r, http.StatusBadRequest)
+			} else if errors.Is(err, models.ErrUserNotFound) {
+				render.Status(r, http.StatusNotFound)
+			}
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		render.JSON(w, r, Token{Token: tokenStr})
 	}
 }
 
@@ -37,12 +80,25 @@ func SignIn() http.HandlerFunc {
 // @Produce  json
 // @Param input body UserInfo true "user's name and password"
 // @Success 200 {object} Token
-// @Failure 400,404 {object} response.Response
+// @Failure 400 {object} response.Response
 // @Failure 500 {object} response.Response
 // @Failure default {object} response.Response
 // @Router /api/v1/sign-up [post]
-func SignUp() http.HandlerFunc {
+func SignUp(authProvider AuthProvider, timeout time.Duration) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		var req UserInfo
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		err = authProvider.SignUp(FromUserInfo(req))
+		if err != nil {
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		render.Status(r, http.StatusOK)
 	}
 }
---
+++ internal/pkg/authUtils/tokenCreator.go
@@ -0,0 +1,99 @@
+package auth_utils
+
+import (
+	"annotater/internal/models"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Payload struct {
+	Login string
+	ID    uuid.UUID
+}
+
+type ITokenHandler interface {
+	GenerateToken(credentials models.User, key string) (string, error)
+	ValidateToken(tokenString string, key string) error
+	ParseToken(tokenString string, key string) (*Payload, error)
+}
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrParsingToken = errors.New("error parsing token")
+)
+
+type JWTTokenHandler struct {
+}
+
+func NewJWTTokenHandler() ITokenHandler {
+	return JWTTokenHandler{}
+}
+
+func (hasher JWTTokenHandler) GenerateToken(credentials models.User, key string) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"exprires": time.Now().Add(time.Hour * 24),
+			"login":    credentials.Login,
+			"ID":       credentials.ID,
+		})
+	tokenString, err := token.SignedString([]byte(key))
+	if err != nil {
+		return "", fmt.Errorf("creating token err: %w", err)
+	}
+
+	return tokenString, nil
+}
+
+func (hasher JWTTokenHandler) ValidateToken(tokenString string, key string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+
+	if err != nil {
+		return ErrParsingToken
+	}
+
+	if !token.Valid {
+		return ErrInvalidToken
+	}
+
+	return nil
+}
+
+func (hasher JWTTokenHandler) ParseToken(tokenString string, key string) (*Payload, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse token")
+	}
+
+	userID, err := uuid.Parse(claims["ID"])
+
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse the ID of user with login %v", claims["login"])
+	}
+
+	payload := &Payload{
+		Login: claims["login"].(string),
+		ID:    userID,
+	}
+
+	return payload, nil
+}
+
+func ExtractTokenFromReq(r *http.Request) string {
+
+	token := r.Header.Get("Authorization")
+	token = strings.TrimPrefix(token, "Bearer ")
+	return token
+}
