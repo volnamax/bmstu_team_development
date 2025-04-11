338595ce0ffd6add83e306686a23a1b58606afa6
Andrew Razin <razin.andrew.ru@mail.ru>
2025-04-11 11:29:54 +0300

    users da[+]: init commit

---
+++ internal/repository/user.go
@@ -0,0 +1,82 @@
+package repository
+
+import (
+	"todolist/internal/adapters"
+	"todolist/internal/models"
+
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID       uuid.UUID `gorm:"primaryKey;column:id;type:uuid;default:gen_random_uuid()"`
+	Name     string    `gorm:"unique;column:name"`
+	Password string    `gorm:"column:password"`
+}
+
+func ToDaUser(user models.UserAuth) User {
+	return User{
+		Name:     user.Name,
+		Password: user.Password,
+	}
+}
+
+func FromDaUser(user User) models.User {
+	return models.User{
+		ID:       user.ID,
+		Name:     user.Name,
+		Password: user.Password,
+	}
+}
+
+type UserRepositoryAdapter struct {
+	db *gorm.DB
+}
+
+func NewUserRepositoryAdapter(srcDB *gorm.DB) adapters.IUserRepository {
+	return &UserRepositoryAdapter{
+		db: srcDB,
+	}
+}
+
+func (repo *UserRepositoryAdapter) GetUserByID(id uuid.UUID) (*models.User, error) {
+	var userDA User
+	userDA.ID = id
+	tx := repo.db.First(&userDA)
+
+	if tx.Error == gorm.ErrRecordNotFound {
+		return nil, models.ErrUserNotFound
+	}
+
+	if tx.Error != nil {
+		return nil, errors.Wrap(tx.Error, "Error getting user by ID")
+	}
+	user := FromDaUser(userDA)
+	return &user, nil
+}
+
+func (repo *UserRepositoryAdapter) GetUserByName(name string) (*models.User, error) {
+	var userDA User
+	tx := repo.db.Where("name = ?", name).First(&userDA)
+
+	if tx.Error == gorm.ErrRecordNotFound {
+		return nil, models.ErrUserNotFound
+	}
+
+	if tx.Error != nil {
+		return nil, errors.Wrap(tx.Error, "Error getting user by ID")
+	}
+	user := FromDaUser(userDA)
+	return &user, nil
+}
+
+func (repo *UserRepositoryAdapter) CreateUser(user *models.UserAuth) error {
+	userDa := ToDaUser(*user)
+	tx := repo.db.Create(&userDa)
+
+	if tx.Error != nil {
+		return errors.Wrap(tx.Error, "error in creating user")
+	}
+	return nil
+}
