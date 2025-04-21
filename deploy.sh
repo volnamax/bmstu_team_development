02abb4f8f346d94718a4f0652654b0508a735e0b
max <mvolnyaga@gmail.com>
2025-04-21 12:05:41 +0300

    update compose

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 set -e  # Остановить скрипт при любой ошибке
 
-echo "🚀 Pull latest image from Docker Hub..."
+echo "🚀 Pulling the latest image from Docker Hub..."
 docker pull volnamax1/todolist:latest
 
-echo "♻️ Recreate containers using docker-compose.prod.yml..."
-docker-compose -f docker-compose.prod.yml up -d
+echo "♻️ Recreating containers using docker-compose.prod.yml..."
+docker-compose -f docker-compose.prod.yml down  # Останавливаем и удаляем старые контейнеры
+docker-compose -f docker-compose.prod.yml up -d  # Перезапускаем контейнеры
 
 echo "✅ Deployment complete! App should be available on port 8080"
